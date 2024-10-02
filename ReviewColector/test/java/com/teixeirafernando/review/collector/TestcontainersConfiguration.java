197bdba489a074dbee8d5cfe18ed4daf5924451b
teixeira-fernando <fernandoadt1@gmail.com>
2024-10-02 14:40:32 +0200

    Deleted files

--- ReviewColector/test/java/com/teixeirafernando/review/collector/TestcontainersConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.teixeirafernando.review.collector;
-
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
-
-}
