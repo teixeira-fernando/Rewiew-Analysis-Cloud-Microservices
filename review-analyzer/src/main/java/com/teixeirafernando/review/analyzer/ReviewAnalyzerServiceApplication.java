45cfc01b5194b289fc7702ef1729a572d9ccbf5e
teixeira-fernando <fernandoadt1@gmail.com>
2024-10-02 17:03:03 +0200

    minimal change to test PR workflows

--- review-analyzer/src/main/java/com/teixeirafernando/review/analyzer/ReviewAnalyzerServiceApplication.java
+++ review-analyzer/src/main/java/com/teixeirafernando/review/analyzer/ReviewAnalyzerServiceApplication.java
@@ -8,7 +8,7 @@ public class ReviewAnalyzerServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReviewAnalyzerServiceApplication.class, args);
-		System.out.println("Application started...");
+		System.out.println("Application started!");
 	}
 
 }
