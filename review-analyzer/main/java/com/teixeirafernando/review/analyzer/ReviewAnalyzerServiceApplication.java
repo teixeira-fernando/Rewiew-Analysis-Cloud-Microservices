5999b92b48feb438be255d70e96bb95c8837a140
teixeira-fernando <fernandoadt1@gmail.com>
2024-10-02 14:24:54 +0200

    Configured initial working multimodules setup

---
+++ review-analyzer/main/java/com/teixeirafernando/review/analyzer/ReviewAnalyzerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.teixeirafernando.review.analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewAnalyzerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewAnalyzerServiceApplication.class, args);
+	}
+
+}
---
+++ review-collector/test/java/com/teixeirafernando/review/collector/TestDataUploderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.teixeirafernando.review.collector;
+
+import org.springframework.boot.SpringApplication;
+
+import java.com.teixeirafernando.review.collector.ReviewCollectorServiceApplication;
+
+public class TestDataUploderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(ReviewCollectorServiceApplication::main).with(com.teixeirafernando.review.collector.TestcontainersConfiguration.class).run(args);
+	}
+
+}
